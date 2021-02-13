6538521d7855ed4718b4bf9d5ec02ec5970f6469
Tony Greeley <tonygreeley@Tonys-iMac.local>
2021-02-13 13:44:26 -0600

    Fetches users from API.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+    
+  useEffect(() => {
+    console.log("useEffect");
+
+    const fetchData = async () => {
+      try{
+        const response = await fetch("https://randomuser.me/api/?results=10");
+        const users = await response.json();
+
+        setUsers([...users.results]);
+      }catch(e){
+        console.error(e);
+      };
+    };
+    fetchData()
+  }, []);
+
+  console.log("users1", users)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hi
     </div>
   );
 }
